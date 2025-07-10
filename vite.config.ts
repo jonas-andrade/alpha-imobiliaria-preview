55b213dbd7f617367ce7fd763bc6bb70ae89ce2a
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-10 19:45:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/alpha-imobiliaria-preview/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
