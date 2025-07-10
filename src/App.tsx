595c5b77d23449837095096de4c4ed345c329193
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-10 19:40:46 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Alterado aqui
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -17,7 +17,7 @@ function App() {
       <div className="min-h-screen bg-black-900 flex flex-col">
         <Header />
 
-        <main className="flex-grow pt-20"> 
+        <main className="flex-grow pt-20">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/propriedades" element={<Properties />} />
