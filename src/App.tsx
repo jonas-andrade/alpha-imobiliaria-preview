cc432767d79c6c96fbc994dbf3b5af8c08974f8b
Jonas Medeiros Andrade <63979333+jonas-andrade@users.noreply.github.com>
2025-07-10 19:48:56 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // Alterado aqui
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'; // <- MUITO IMPORTANTE
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
